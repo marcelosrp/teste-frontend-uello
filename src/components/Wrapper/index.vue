5b8fa794176a38dbb8585aa4787e40790055c5db
Marcelo Soares <soares.srp@gmail.com>
2021-02-05 13:43:03 -0300

    tratando os erros da api geolocalização

--- src/components/Wrapper/index.vue
+++ src/components/Wrapper/index.vue
@@ -4,20 +4,27 @@
     <div class="d-none d-sm-block">
       <Login :isMobile="false" name="hirota food express headquarters" />
     </div>
-    <Tracking />
 
-    <div v-if="errorStr">
-      Sorry, but the following error
-      occurred: {{errorStr}}
+    <Tracking />
+    
+    <div v-if="error" class="wrapper-messages">
+      <h1 class="error-title">
+        {{ error }}
+      </h1>
     </div>
     
-    <div v-if="gettingLocation">
-      <i>Getting your location...</i>
+    <div class="wrapper-messages" v-if="gettingLocation">
+      <b-spinner
+        :variant="variant"
+        :key="variant"
+      ></b-spinner>
     </div>
-    
-    
-      <Mapa v-if="location" :lon="location.coords.longitude" :lat="location.coords.latitude" />
-    
+
+    <Mapa 
+      v-if="location" 
+      :lon="location.coords.longitude" 
+      :lat="location.coords.latitude"
+    />
   </main>
 </template>
 
@@ -27,34 +34,42 @@
   import Mapa from "../Mapa";
 
   export default {
-    data () {
-      return {
-        location: null,
-        gettingLocation: false,
-        errorStr: null
-      }
-    },
-    created() {
-      //do we support geolocation
-      if(!("geolocation" in navigator)) {
-        this.errorStr = 'Geolocation is not available.';
-        return;
-      }
-
-      this.gettingLocation = true;
-      // get position
-      navigator.geolocation.getCurrentPosition(pos => {
-        this.gettingLocation = false;
-        this.location = pos;
-      }, err => {
-        this.gettingLocation = false;
-        this.errorStr = err.message;
-      })
-    },
     components: {
       Login,
       Tracking,
       Mapa
+    },
+    data () {
+      return {
+        location: null,
+        gettingLocation: false,
+        error: null,
+        variant: "danger"
+      }
+    },
+    created() {
+      if(!("geolocation" in navigator)) {
+        this.error = 'Geolocation is not available.';
+        return;
+      }
+
+      this.gettingLocation = true;
+      
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.gettingLocation = false;
+        this.location = pos;
+      }, err => {
+        if(err.code === 1) {
+          this.gettingLocation = false;
+          this.error = "Você precisa dar permissão para o navegador acessar sua localização atual.";
+        } else if(err.code === 3) {
+          this.gettingLocation = false;
+          this.error = "A rede está fora do ar ou o serviço de posicionamento não pode ser alcançado. Atualize a página para tentar novamente.";
+        } else {
+          this.gettingLocation = false;
+          this.error = "Não foi possível pegar a sua localização atual. Atualize a página para tentar novamente"
+        }
+      })
     }
   }
 </script>
@@ -66,5 +81,19 @@
     padding: 3.75rem 1.25rem;
     position: relative;
     width: 100%;
+  }
+
+  .wrapper-messages {
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .error-title {
+    font-size: 1.25rem;
+    text-align: center;
+    width: 60%;
   }
 </style>
